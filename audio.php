72a3358819bcf4c309db94130d6a0949ff4f17af
DIVYABHARATHI-L <119557063+DIVYABHARATHI-L@users.noreply.github.com>
2025-07-06 07:37:19 +0530

    Add files via upload

---
+++ audio.php
@@ -0,0 +1,50 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ration Shop Audio Recorder</title>
+    <link rel="stylesheet" href="astyle.css">
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <h1>Ration Shop Audio Recorder</h1>
+            </div>
+    </header>
+    <main class="main">
+        <div class="container">
+            <section class="recording-section">
+                <h2>Record Your Audio</h2>
+                <div class="display">
+                    <ul id="audio-list"></ul>
+                </div>
+                <div class="controllers">
+                    <button id="record-button" style=" color:green"class="material-icons">mic</button>
+                    <button id="stop-button" style="color:red" class="material-icons" disabled>stop</button>
+                    <button id="download-button" class="material-icons" disabled>file_download</button>
+                </div>
+            </section>
+        </div>
+    </main>
+    <footer class="footer">
+        <div class="container">
+            <p>&copy; 2024 Ration Shop. All Rights Reserved.</p>
+        </div>
+    </footer>
+    <script src="ascript.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+     include("connection.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+        <h1 class="main-title">RATION.IO</h1>
+        <h2 class="login-title">Login</h2>
+        <form action="login.php" method="post" >
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="button-container">
+                <button type="submit" name="submit">Login</button>
+        </form>
+    </div>
+</body>
+
